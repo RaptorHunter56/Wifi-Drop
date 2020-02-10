dd843960bcec801baec7362a99c6bc51f6da02c9
Steven <sb.nb.lego@Gmail.com>
2020-02-11 10:49:05 +1300

    Add project files.

---
+++ Wifi Drop/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+namespace Wifi_Drop
+{
+    class Program
+    {
+        private void AvailabilityChanged(object sender, NetworkAvailabilityEventArgs e)
+        {
+            if (e.IsAvailable)
+                Console.WriteLine("Network connected!");
+            else
+                Console.WriteLine("Network dis connected!");
+        }
+        public void Open()
+        {
+            NetworkChange.NetworkAvailabilityChanged += AvailabilityChanged;
+        }
+
+        private void Close()
+        {
+            NetworkChange.NetworkAvailabilityChanged -= AvailabilityChanged;
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Open();
+            Console.ReadLine();
+            p.Close();
+        }
+    }
+}
